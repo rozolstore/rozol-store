31c211bbc97c8b05e0dcaecbef2ffe3e04499264
ROZOL <rozol.store@gmail.com>
2026-07-24 08:21:28 +0300

    Add Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
+      <GoogleAnalytics gaId="G-XL3KD18BN5" />
     </html>
   );
 }
